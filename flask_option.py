78fe958f561802b04d3cf220e46f31cfe6e0e0d3
francois GIRARD <francoisgirard51@gmail.com>
2023-10-24 12:48:30 +0200

    FLASK_ENV now returns string (values)

--- flask_option.py
+++ flask_option.py
@@ -4,7 +4,13 @@ import os
 
 def start():
     """returns the right message"""
-    pass  # YOUR CODE HERE
+    flask_env = os.environ.get("FLASK_ENV")
+
+    if flask_env == "development":
+        return "Starting in development mode..."
+    if flask_env == "production":
+        return "Starting in production mode..."
+    return "Starting in empty mode..."
 
 if __name__ == "__main__":
     print(start())
